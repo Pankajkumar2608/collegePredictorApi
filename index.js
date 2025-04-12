1ece954ccf0c2c7d823b3ab3efb9471b5f83e53d
Pankaj jaat <125138274+Pankajkumar2608@users.noreply.github.com>
2025-04-12 19:36:30 +0530

    its desc not dsc

--- index.js
+++ index.js
@@ -260,7 +260,7 @@ app.post('/filter', async (req, res) => {
         if (userRank) {
             filterQuery += ` AND NULLIF("Opening Rank", '')::INTEGER <= $1 
                             AND NULLIF("Closing Rank", '')::INTEGER >= $1`;
-            filterQuery += ` ORDER BY rank_diff DSC `;
+            filterQuery += ` ORDER BY rank_diff DESC `;
         } else {
             filterQuery += ` ORDER BY NULLIF("Opening Rank", '')::INTEGER ASC `;
         }
